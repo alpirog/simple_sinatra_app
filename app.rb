b55e0c2b00f43bf738d90091dd77a0e7856b4ec5
alpirog <varduzed@gmail.com>
2019-11-20 11:47:45 +0300

    Initial app

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @height = params[:height].to_f
+  @weight = params[:weight].to_f
+  if params[:height] == '' || params[:weight] == ''
+    @input_error = "Field can't be empty! Try again."
+    erb :index
+  else
+    @bmi = (@weight / ((@height / 100) ** 2)).round(2)
+
+    f = File.open('bmis.txt', 'a')
+    f.write "Height: #{@height}; weight: #{@weight}; BMI: #{@bmi}\n"
+    f.close
+
+    erb :bmi
+  end
+end
+
+get '/tdee' do
+  under_construction
+  erb :message
+end
+
+get '/about' do
+  under_construction
+  erb :message
+end
+
+get '/show_bmi' do
+  @file = File.open("./bmis.txt", "r")
+  erb :show_bmi
+end
+
+def under_construction
+  @message = "This page is under construcion."
+end
